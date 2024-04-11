2316794e39e5917470bd73c430b3f5f9a09e3e3f
unknown <joellie@albert2020>
2024-04-11 13:29:26 +0200

    deuxieme commit

--- database/migrations/2024_04_11_104451_create_products_table.php
+++ database/migrations/2024_04_11_104451_create_products_table.php
@@ -15,7 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->float('priceHt');
+            $table->dateTime('creationDate');
+            $table->dateTime('dateUpdate')->nullable();
         });
     }
 
